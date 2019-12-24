cc90919287c0012589e83c8481d34ecf42456593
David Thaler <davidthaler@gmail.com>
2019-12-24 00:36:50 -0800

    Reorganized the listing and minor cleanup.

--- graphMaker.js
+++ graphMaker.js
@@ -3,6 +3,14 @@ Third take on graphMaker.
 */
 window.onload = main;
 
+let nodeMap = new Map();
+let edgeMap = new Map();
+let s, t;
+const RADIUS = 10;
+const FONTSIZE = 10;
+const OFFSET = 3;
+let state = 'none';
+
 // Name and ID generators
 getID = function(){let i=0;return function(){return i++}}();
 getName = function(){
@@ -14,15 +22,7 @@ getName = function(){
         });
     }();    //evaluated
 
-
-let nodeMap = new Map();
-let edgeMap = new Map();
-let s, t;
-const RADIUS = 10;
-const FONTSIZE = 10;
-const OFFSET = 3;
-let state = 'none';
-
+// Data operations
 function addNode(x, y, name=getName()){
     let newId = getID();
     nodeMap.set(newId, {name, id:newId, x, y, edges:[]});
@@ -57,23 +57,7 @@ function moveNode(nodeId, dx, dy){
     node.y += dy;
 }
 
-function node(sel){
-    sel.append('circle').attr('r', RADIUS)
-        .attr('cx', 0).attr('cy', 0)
-        .attr('fill', 'darkgrey')
-        .classed('nodeCircle', true);
-
-    sel.append('text').attr('x', 0).attr('y', OFFSET)
-        .attr('text-anchor', 'middle').attr('font-size', FONTSIZE)
-        .attr('font-family', 'sans-serif')
-        .classed('nodeLabel', true)
-        .text(d => d.name);
-}
-
-function translate(d){
-    return `translate(${d.x},${d.y})`;
-}
-
+// Event handlers
 function clickNode(){
     let nodeId = Number(this.getAttribute('nodeId'));
     if(state == 'removeNode'){
@@ -94,6 +78,31 @@ function clickNode(){
     }
 }
 
+function clickEdge(){
+    if(state=='removeEdge'){
+        removeEdge(this.getAttribute('edgeId'));
+        drawGraph();
+    }
+}
+
+// D3 graphics
+function node(sel){
+    sel.append('circle').attr('r', RADIUS)
+        .attr('cx', 0).attr('cy', 0)
+        .attr('fill', 'darkgrey')
+        .classed('nodeCircle', true);
+
+    sel.append('text').attr('x', 0).attr('y', OFFSET)
+        .attr('text-anchor', 'middle').attr('font-size', FONTSIZE)
+        .attr('font-family', 'sans-serif')
+        .classed('nodeLabel', true)
+        .text(d => d.name);
+}
+
+function translate(d){
+    return `translate(${d.x},${d.y})`;
+}
+
 function makeNodes(nodes){
     let nodeSel = d3.select('svg').selectAll('g').data(nodes, d => d.id);
     nodeSel.exit().remove();
@@ -111,13 +120,6 @@ function makeNodes(nodes){
                           }));
     nodeSel = newNodes.merge(nodeSel);
     nodeSel.attr('transform', translate).raise();
-}
-
-function clickEdge(){
-    if(state=='removeEdge'){
-        removeEdge(this.getAttribute('edgeId'));
-        drawGraph();
-    }
 }
 
 function makeEdges(edges){
@@ -156,10 +158,9 @@ function main(){
     d3.select('#removeNode').on('click', buttonClick('removeNode'));
     d3.select('#addEdge').on('click', buttonClick(['pickS', 'pickT']));
     d3.select('#removeEdge').on('click', buttonClick('removeEdge'));
-    drawGraph();
     d3.select('svg').on('click', function(){
-        let [x, y] = d3.mouse(this);
         if(state == 'addNode'){
+            let [x, y] = d3.mouse(this);
             addNode(x, y);
             drawGraph();
         }
