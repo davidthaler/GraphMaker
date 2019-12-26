7b32e5980b3a8b5ff9fddf41b3cf616ef7d8b3f4
David Thaler <davidthaler@gmail.com>
2019-12-25 23:27:24 -0800

    Initial more-or-less working animated BFS. It is too fast to see outside the debugger and does not yet animate edges.

--- bfs.js
+++ bfs.js
@@ -3,21 +3,34 @@ Using BFS as a first cut at an algorithm to run on graphMaker.
 Explores the connected component from startNode
 */
 
+const nodeStates = {'unexplored': 'darkgray',
+                    'visited'   : 'red',
+                    'completed' : 'darkslategray'};
+
+const edgeStates = {'unexplored': 'lightgray',
+                    'visited'   : 'pink',
+                    'completed' : 'black'};
+
 function BFS(g, startNodeName){
+    g.fillEdgeProperty('stateColor', edgeStates.unexplored);
+    g.fillNodeProperty('stateColor', nodeStates.unexplored);
+    drawGraph();
+    g.fillNodeProperty('visited', false);
     let startNode = g.getNodeByName(startNodeName);
     let Q = [startNode];
     startNode.visited = true;
+    startNode.stateColor = nodeStates.visited;
     while(Q.length > 0){
         let currentNode = Q.shift();
-        console.log(`Expanding ${currentNode.name}`);
         for(let n of g.neighbors(currentNode)){
             if(!n.visited){
                 Q.push(n);
                 n.visited = true;
-                console.log(`enqueueing node ${n.name}`);
+                n.stateColor = nodeStates.visited;
+                drawGraph();
             }
         }
-        console.log(`Completing ${currentNode.name}`);
+        currentNode.stateColor = nodeStates.completed;
+        drawGraph();
     }
-    console.log(`Completed BFS starting from node ${startNode.name}`);
 }
