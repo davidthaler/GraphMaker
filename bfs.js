c884f6282537d4182c7047997492b28e96bb305f
David Thaler <davidthaler@gmail.com>
2019-12-25 21:34:23 -0800

    Modified BFS to work with the new Graph class (w/o Node and Edge). I like this better. Currently runs on node names, which is easier to smoke test, but might have to change that to nodeId to use it in graphMaker from the UI.

--- bfs.js
+++ bfs.js
@@ -3,19 +3,19 @@ Using BFS as a first cut at an algorithm to run on graphMaker.
 Explores the connected component from startNode
 */
 
-function BFS(startNode){
+function BFS(g, startNodeName){
+    let startNode = g.getNodeByName(startNodeName);
     let Q = [startNode];
     startNode.visited = true;
     while(Q.length > 0){
         let currentNode = Q.shift();
         console.log(`Expanding ${currentNode.name}`);
-        for(let n of currentNode.neighbors()){
+        for(let n of g.neighbors(currentNode)){
             if(!n.visited){
                 Q.push(n);
                 n.visited = true;
                 console.log(`enqueueing node ${n.name}`);
             }
-
         }
         console.log(`Completing ${currentNode.name}`);
     }
